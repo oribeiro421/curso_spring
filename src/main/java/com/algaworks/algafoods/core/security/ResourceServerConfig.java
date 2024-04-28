64134f37e3a0bfb91133de0f195ee8915d9b94c2
azship14 <otavioribeiro421@gmail.com>
2024-04-28 09:46:59 -0300

    commit 2804

---
+++ src/main/java/com/algaworks/algafoods/core/security/ResourceServerConfig.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafoods.core.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                .and()
+                    .oauth2ResourceServer().opaqueToken();
+    }
+}
